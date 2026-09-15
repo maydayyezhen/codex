e363c6351c29f7320914c102de103ac7327f6256
Zhang Zihao <162564657+maydayyezhen@users.noreply.github.com>
2026-09-16 00:06:11 +0800

    feat: connect module selection to character selection

--- src/pages/NewGamePage.tsx
+++ src/pages/NewGamePage.tsx
@@ -1,11 +1,16 @@
 import { ModuleCarousel } from '../components/ModuleCarousel'
 import { PageShell } from '../components/PageShell'
 import { gameModules } from '../data/modules'
+import type { GameModule } from '../types'
 
-export function NewGamePage() {
+interface NewGamePageProps {
+  onContinue: (module: GameModule) => void
+}
+
+export function NewGamePage({ onContinue }: NewGamePageProps) {
   return (
     <PageShell path="LORE / NEW GAME" title="选择模组" bodyClassName="new-game-page">
-      <ModuleCarousel modules={gameModules} />
+      <ModuleCarousel modules={gameModules} onContinue={onContinue} />
     </PageShell>
   )
 }
